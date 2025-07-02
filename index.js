6dd4acc7b2ed86886a669821f967e66a42417188
securedeviceskarnataka <securedeviceskarnataka@gmail.com>
2025-07-02 17:34:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,67 +1,54 @@
 const mineflayer = require('mineflayer');
 
 function createBot() {
-const bot = mineflayer.createBot({
-host: 'sudana_smp.aternos.me', // SERVER IP
-username: 'SUDANA_24X7',     // BOT NAME
-port: 53659,                   // SERVER PORT
-version: '1.16.5',
-});
+  const bot = mineflayer.createBot({
+    host: 'sudana_smp.aternos.me', // SERVER IP
+    username: 'SUDANA_24X7',     // BOT NAME
+    port: 53659,                   // SERVER PORT
+    version: '1.16.5',
+  });
 
-bot.on('spawn', () => {
-bot.chat('/register aagop04');
-});
+  bot.on('spawn', () => {
+    bot.chat('/register aagop04');
+  });
 
-bot.on("move", function () {
-bot.setControlState("jump", true);
-setTimeout(() => {
-bot.setControlState("jump", false);
-}, 1000);
+  bot.on("move", function () {
+    bot.setControlState("jump", true);
+    setTimeout(() => bot.setControlState("jump", false), 1000);
 
-setTimeout(() => {  
-  bot.setControlState("forward", true);  
-  setTimeout(() => {  
-    bot.setControlState("forward", false);  
-  }, 500);  
-}, 1000);  
+    setTimeout(() => {
+      bot.setControlState("forward", true);
+      setTimeout(() => bot.setControlState("forward", false), 500);
+    }, 1000);
 
-setTimeout(() => {  
-  bot.setControlState("back", true);  
-  setTimeout(() => {  
-    bot.setControlState("back", false);  
-  }, 500);  
-}, 2000);  
+    setTimeout(() => {
+      bot.setControlState("back", true);
+      setTimeout(() => bot.setControlState("back", false), 500);
+    }, 2000);
 
-setTimeout(() => {  
-  bot.setControlState("right", true);  
-  setTimeout(() => {  
-    bot.setControlState("right", false);  
-  }, 2000);  
-}, 500);  
+    setTimeout(() => {
+      bot.setControlState("right", true);
+      setTimeout(() => bot.setControlState("right", false), 2000);
+    }, 500);
 
-setTimeout(() => {  
-  bot.setControlState("left", true);  
-  setTimeout(() => {  
-    bot.setControlState("left", false);  
-  }, 2000);  
-}, 500);
+    setTimeout(() => {
+      bot.setControlState("left", true);
+      setTimeout(() => bot.setControlState("left", false), 2000);
+    }, 500);
+  });
 
-});
+  bot.on('kicked', console.log);
+  bot.on('error', console.log);
+  bot.on('end', () => {
+    console.log("Bot disconnected. Reconnecting in 1 minute 50 seconds...");
+    setTimeout(createBot, 110000); // 1 min 50 sec = 110000 ms
+  });
 
-bot.on('kicked', console.log);
-bot.on('error', console.log);
-bot.on('end', createBot);
-
-// Auto-disconnect and reconnect every 3 hours
-setInterval(() => {
-console.log("Reconnecting bot to avoid Aternos ban...");
-bot.quit();
-
-setTimeout(() => {  
-  createBot();  
-}, 60000 ); // 1min  wait before reconnect
-
-}, 3 * 60 * 60 * 1000); // 3 hours
+  // Force restart every 1 hour
+  setTimeout(() => {
+    console.log("Forcing bot to leave after 1 hour...");
+    bot.quit();
+  }, 60 * 60 * 1000); // 1 hour = 3600000 ms
 }
 
 createBot();
